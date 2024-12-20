5af02c4b8ea7ee195bc688d9cca48a0c9d67a817
Leonardo Anjos <leodrums11@gmail.com>
2024-12-20 06:52:42 -0300

    feat: disable seach, login and cart

--- src/ui/components/nav/Nav.tsx
+++ src/ui/components/nav/Nav.tsx
@@ -11,19 +11,19 @@ export const Nav = ({ channel }: { channel: string }) => {
 			<ul className="hidden gap-4 overflow-x-auto whitespace-nowrap md:flex lg:gap-8 lg:px-0">
 				<NavLinks channel={channel} />
 			</ul>
-			<div className="ml-auto flex items-center justify-center gap-4 whitespace-nowrap lg:gap-8">
+			{/* <div className="ml-auto flex items-center justify-center gap-4 whitespace-nowrap lg:gap-8">
 				<div className="hidden lg:flex">
 					<SearchBar channel={channel} />
 				</div>
 				<Suspense fallback={<div className="w-8" />}>
 					<UserMenuContainer />
 				</Suspense>
-			</div>
-			<div className="flex items-center">
+			</div> */}
+			{/* <div className="flex items-center">
 				<Suspense fallback={<div className="w-6" />}>
 					<CartNavItem channel={channel} />
 				</Suspense>
-			</div>
+			</div> */}
 			<Suspense>
 				<MobileMenu>
 					<SearchBar channel={channel} />
